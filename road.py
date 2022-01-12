840905375be9d9be5ad336d6caa662061ac3616d
Lars Janssen <janssen.lars@gmail.com>
2022-01-12 13:48:40 +0100

    Added some comments

--- road.py
+++ road.py
@@ -4,11 +4,17 @@ import numpy as np
     Defines a road by its start and end points.
 """
 class Road:
-    def __init__(self, start, end):
+    """
+        Sets the start parameters:
+        start, end, id, length and angle
+    """
+    def __init__(self, start, end, id):
         self.start = start
         self.end = end
-        self.length = np.sqrt((start[0] - end[0]) ** 2 + (start[1] - end[1]) ** 2)
+        self.id = id
 
         deltaX = end[0] - start[0]
         deltaY = end[1] - start[1]
+        self.length = np.sqrt(deltaX ** 2 + deltaY ** 2)
+        #in pygame, y is going down, so we have to invert it
         self.angle = -np.arctan2(deltaY, deltaX)
